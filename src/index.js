781db44d4904809931c1804fb6a8e13d27bea73a
Subrata99999 <subratamajumder284@gmail.com>
2025-04-03 19:34:35 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import {connectDB} from "../lib/db.js";
 import authRoutes from "../routes/auth.route.js";
+import messageRoutes from "../routes/message.route.js";
 
 
 dotenv.config()
@@ -12,12 +15,18 @@ const app = express();
 const PORT = process.env.PORT
 
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin: "htttp://localhost:1575",
+    Credential: true
+}));
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT,()=>
 {
-    console.log("server is running on PORT:"+ PORT);
+    console.log(`Server running on port ${PORT}`);
     connectDB();
 });
 
